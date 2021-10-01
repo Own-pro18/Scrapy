bd63c9fb82b92a686e2ddcf96329463eb9d9ea4c
karthi2018 <thirumalkarthiga2018@gmail.com>
2021-10-01 23:50:08 +0100

    initial

---
+++ How to scraping with BeautifulSoup_htmlparser.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+def scrap():
+    page = requests.get('https://www.example.co.uk')
+    contents = page.content
+    soup = BeautifulSoup(contents, 'html.parser')
+    return (soup.find_all('a'))
+
+
+print(scrap())
